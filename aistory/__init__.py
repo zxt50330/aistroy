7b74c6e70ea816d79b9c9f6c09d483bdff31f0e0
xtzhao <zxt50330@gmail.com>
2020-06-09 16:25:14 +0800

    init

---
+++ aistory/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# © 2018 QYT Technology
+# Authored by: Zhao Xingtao (zxt50330@gmail.com)
---
+++ aistory/story_resource/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StoryResourceConfig(AppConfig):
+    name = 'story_resource'
