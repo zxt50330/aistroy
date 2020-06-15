ca667c8590ab2f02d3e7a09660f6399a5e5b69ee
zhao <zxt50330@gmail.com>
2020-06-15 22:40:20 +0800

    play mp3

--- aistory/story_resource/tests.py
+++ aistory/story_resource/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+import vlc
+import time
 # Create your tests here.
+
+
+p = vlc.MediaPlayer("/Users/zhao/PycharmProjects/aistroy/aistory/story_resource/1.mp3")
+p.play()
+
+
+while True:
+    time.sleep(1)
+    print(p.get_time())
+    p.pause()
+    p.pause()
+    p.audio_set_volume(50)
+    p.audio_get_volume()
